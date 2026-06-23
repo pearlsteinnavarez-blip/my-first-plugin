1a0e7dd32b86731ee84209be1304c127f0b15604
胡国明 <huguomin@MacBook-Air.local>
2026-06-23 16:01:13 +0800

    实现表格内 Enter 插入 br

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { Plugin } from 'obsidian';
 import { createTableCellBrExtension } from './editor/tableCellBrExtension';
+import { registerScrollPositionMemory } from './features/scrollPositionMemory';
 
 export default class TableCellBrPlugin extends Plugin {
 	async onload() {
 		this.registerEditorExtension(createTableCellBrExtension());
+		registerScrollPositionMemory(this);
 	}
 }
